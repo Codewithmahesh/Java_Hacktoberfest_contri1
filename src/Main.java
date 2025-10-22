34521bd8b329b498541b664eb5aa43c4d3bd8bbc
Codewithmahesh <girimahesh1514@gmail.com>
2025-10-22 17:07:59 +0530

    Enhanced random number generator and updated README

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,32 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args) {
+        // List of numbers to pick from
+        int[] numbers = {5, 4, 8, 9, 6, 3, 7, 4, 2, 85, 63, 45, 10};
 
-        int[] numbers = new int[]{5,4,8,9,6,3,7,4,2,85,63,45,10};
+        // Show all possible numbers
+        System.out.print("Possible numbers: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
         Random random = new Random();
-        int ranNum = random.nextInt(numbers.length-1);
-        int a = numbers[ranNum];
-        System.out.print("Your generated random number is:  " + a);
+        Scanner scanner = new Scanner(System.in);
 
+        String choice;
+        do {
+            int randomIndex = random.nextInt(numbers.length);
+            int result = numbers[randomIndex];
+            System.out.println("🎲 Your generated random number is: " + result);
+
+            System.out.print("Do you want to generate another number? (yes/no): ");
+            choice = scanner.nextLine().trim().toLowerCase();
+        } while (choice.equals("yes"));
+
+        System.out.println("Thank you for using the Random Number Generator!");
+        scanner.close();
     }
 }
